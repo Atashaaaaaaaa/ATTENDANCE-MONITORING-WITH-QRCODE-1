d05a8711c2769b6bf60c8abc023ae5789ba7f27e
User <user@example.com>
2026-04-10 14:12:45 +0800

    added firebase api key

--- ams-app/src/lib/firebase.js
+++ ams-app/src/lib/firebase.js
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA1indZCIm_VfhJpdL0pPS_DNXA5mgoWOg",
+  authDomain: "a-m-s-27607.firebaseapp.com",
+  projectId: "a-m-s-27607",
+  storageBucket: "a-m-s-27607.firebasestorage.app",
+  messagingSenderId: "1033675043994",
+  appId: "1:1033675043994:web:aadd00a2d18dcd880f1b23",
+  measurementId: "G-VC12THY8SJ"
 };
 
 const app = initializeApp(firebaseConfig);
